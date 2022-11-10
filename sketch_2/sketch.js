9eea13fe187dfa66cc7190e25ba0a5ad15e527b9
molmax <molmax@users.noreply.github.com>
2022-11-10 17:15:05 +0400

    Thy Food Consumed

--- sketch_2/sketch.js
+++ sketch_2/sketch.js
@@ -1,18 +1,50 @@
-const canvasWidth = 800;
-const canvasHeight = 600;
-let squareX = 0;
-let squareY = 200;
+//Thy Food Consumed
+
+const canvasWidth = 400;
+const canvasHeight = 400;
+const rectSide = 15;
+const randOffset = 10;
+const frameRateValue = 60;
+const bckgColor = 120;
+let heroX = 0;
+let heroY = canvasHeight / 2;
+let heroWidth = rectSide;
+let heroHeight = rectSide;
+let heroSpeed = 1;
+let foodX = 0;
+let foodY = 0;
+let thyFoodConsumed = false;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  frameRate(60);
+  frameRate(frameRateValue);
 }
 
 function draw() {
-  background(120);
-  if (squareX > canvasWidth) {
-    squareX = 0;
+  background(bckgColor);
+  
+  if (heroX > canvasWidth) {
+    heroX = 0;
+    heroWidth = rectSide;
+    thyFoodConsumed = false;
   }
-  squareX = squareX + 1;
-  rect(squareX, squareY, 9);
+  
+  if(heroX == 0) {
+    randX = random(randOffset, canvasWidth - randOffset);
+    foodX = randX - randX % 1; //Yeah, I know
+    foodY = canvasHeight / 2;
+  }
+
+  if (!thyFoodConsumed) {
+    rect(foodX, foodY, rectSide); 
+  }
+  
+  heroX = heroX + heroSpeed;
+  
+  if(heroX == foodX) {
+    thyFoodConsumed = true;
+    heroWidth = heroWidth + rectSide
+  }
+
+  rect(heroX, heroY, heroWidth, heroHeight);
 }
