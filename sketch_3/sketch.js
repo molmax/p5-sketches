61a37f2af5d47ae637946cc67f8f727908c1d9d1
molmax <molmax@users.noreply.github.com>
2023-01-26 15:42:53 +0300

    Sketch#3. Add more walkers.

--- sketch_3/sketch.js
+++ sketch_3/sketch.js
@@ -2,23 +2,29 @@ const canvasWidth = 400;
 const canvasHeight = 400;
 
 function setup() {
-  w = new Walker(canvasHeight / 2, canvasWidth / 2);
   createCanvas(canvasWidth, canvasHeight);
   background(0);
   frameRate(60);
+
+  wGreen = new Walker(canvasHeight / 2, canvasWidth / 2, new Color(255, 244, 0));
+  wYellow = new Walker(canvasHeight / 2 - 100 , canvasWidth / 2 + 100, new Color(0, 255, 0));
+  wRed = new Walker(canvasHeight / 2 + 100 , canvasWidth / 2 - 100, new Color(255, 0, 0));
+
+  walkers = [wGreen, wYellow, wRed];
 }
 
 function draw() {
-  w.step();
-  stroke(255, 244, 0)
-  strokeWeight(3);
-  w.render();
+  walkers.forEach(w => {
+    w.step();
+    w.render();
+  });
 }
 
 class Walker {
-  constructor(x, y) {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
+    this.color = color;
   }
 
   step() {
@@ -28,5 +34,15 @@ class Walker {
 
   render() {
     point(this.x, this.y);
+    stroke(this.color.r, this.color.g, this.color.b);
+    strokeWeight(3);
+  }
+}
+
+class Color {
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
   }
 }
